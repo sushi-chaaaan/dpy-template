4bdaa09f17feeb5b3f4e116c51bad9f9cb7198cc
sushi-chaaaan <mail@sushichan.live>
2023-03-11 20:22:10 +0900

    add logging feature for AppCommand and Ext.Command

---
+++ const.py
@@ -0,0 +1,17 @@
+APP_COMMAND_LOG_FORMAT = """
+[Interaction Command]
+Command Name: {command_name}
+Guild ID: {guild_id}
+Channel ID: {channel_id}
+Author ID: {author_id}
+Author Name: {author_name}
+"""
+
+COMMAND_LOG_FORMAT = """
+[Command]
+Command Name: {command_name}
+Guild ID: {guild_id}
+Channel ID: {channel_id}
+Author ID: {author_id}
+Author Name: {author_name}
+"""
